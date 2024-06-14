13f015d145ee479009deb8d75fcb8b00cc1fda3c
Xterminal1 <124107188+Xterminal1@users.noreply.github.com>
2024-06-14 14:42:12 -0700

    Added luminosity calc and log functions

--- calc.py
+++ calc.py
@@ -1,33 +1,49 @@
+import math
 from math import log10
 
 # solar constants
-mVSun = 4.83 # absolute magnitude
-tSun = 5772 # nominal effective temperature, Kelvin
-lSun = 3.0128 # luminosity, Watts
+M_sun = 4.83 # absolute magnitude
+T_sun = 5772 # effective temperature [K]
+L_sun = 3.0128 # luminosity [W]
 
-def l_tEff(temperature, luminosity):
-    return ((tSun/temperature)**4 * luminosity)**0.5
+def L_R_Teff(calc: int, radius, temp, lum):
+    if calc == 1:
+        return ((T_sun/temp)**4 * lum)**0.5
+    elif calc == 2:
+        return (radius/((T_sun/temp)**2))**2
+    elif calc == 3:
+        return T_sun/(radius/lum**0.5)**0.5
+    else:
+        return 'ERROR!!!'
 
-def AD(distance, angular_diameter):
-    return distance * angular_diameter/1000 * 107.5
+def AD(dist, angular_diameter):
+    return dist * angular_diameter/1000 * 107.5
 
-print()
-print('Xterminal1 : Stellar Calculator')
+def log(param):
+    return log10(param/1)
 
 while 1:
+    print('-----------------------------------------------------------')
+    print('       Xterminal1 : Stellar characteristics calculator')
+    print('-----------------------------------------------------------')
+
     calcs = [
-        'Radius'
+        'Radius', 'Luminosity'
     ]
     for count, ele in enumerate(calcs, 1):
         print(count, ele)
     print()
-    calc = int(input('>>> '))
+    calc = input('>>> ')
+    if calc.lower() == 'exit':
+        print('EXITING...')
+        break
 
-    match calc:
+    match int(calc):
+
         # Radius
         case 1:
             radius_calcs = [
-                'Luminosity-effective temperature', 'Distance-angular diameter', 'Parallax-angular diameter'
+                'Luminosity-effective temperature', 'Distance-angular diameter', 'Parallax-angular diameter', 'log(R/R☉)'
             ]
             for count, ele in enumerate(radius_calcs, 1):
                 print(count, ele)
@@ -37,20 +53,43 @@ while 1:
             match radius_calc:
                 # Luminosity-effective temperature
                 case 1:
-                    radius = l_tEff(float(input('Effective temperature (Kelvin): ')), float(input('Luminosity (L☉): ')))
+                    radius = L_R_Teff(1, 0, float(input('Effective temperature [K]: ')), float(input('Luminosity [L☉]: ')))
                     print()
-                    print(f'Radius (R☉): {radius}')
+                    print(f'Radius [R☉]: {radius}')
 
                 # Distance-angular diameter
                 case 2:
-                    radius = AD(float(input('Distance (parsecs): ')), float(input('Angular diameter (milliarcseconds): ')))
+                    radius = AD(float(input('Distance [pc]: ')), float(input('Angular diameter [mas]: ')))
                     print()
-                    print(f'Radius (R☉): {radius}')
+                    print(f'Radius [R☉]: {radius}')
 
                 # Parallax-angular diameter
                 case 3:
-                    radius = AD(1/float(input('Parallax (milliarcseconds): '))*1000, float(input('Angular diameter (milliarcseconds): ')))
+                    radius = AD(1/float(input('Parallax [mas]: '))*1000, float(input('Angular diameter [mas]: ')))
                     print()
-                    print(f'Radius (R☉): {radius}')
+                    print(f'Radius [R☉]: {radius}')
+
+                # log(R/R☉)
+                case 4:
+                    log_radius = log(float(input('Radius [R☉]: ')))
+                    print()
+                    print(f'log(R/R☉): {log_radius}')
 
             print()
+
+        # Luminosity
+        case 2:
+            lum_calcs = [
+                'Radius-effective temperature', 'log(L/L☉)'
+            ]
+            for count, ele in enumerate(lum_calcs, 1):
+                print(count, ele)
+            print()
+            lum_calc = int(input('>>> '))
+
+            match lum_calc:
+                # Radius-effective temperature
+                case 1:
+                    lum = L_R_Teff(2, float(input('Radius [R☉]: ')), float(input('Effective temperature [K]: ')), 0)
+                    print()
+                    print(f'Luminosity [L☉]: {lum}')
