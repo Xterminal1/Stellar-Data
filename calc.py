0a6d535f88bc2e95d3f568902e077560e6c01eb2
Xterminal1 <124107188+Xterminal1@users.noreply.github.com>
2024-06-16 22:41:00 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,29 +2,37 @@ import math
 from math import log10
 
 # solar constants
-M_sun = 4.83 # absolute magnitude
-T_sun = 5772 # effective temperature [K]
+Mbol_sun = 4.74 # absolute magnitude
+Teff_sun = 5772 # effective temperature [K]
 L_sun = 3.0128 # luminosity [W]
 
-def L_R_Teff(calc: int, radius, temp, lum):
+def LRT(calc: int, radius, Teff, lum):
     if calc == 1:
-        return ((T_sun/temp)**4 * lum)**0.5
+        return ((Teff_sun/Teff)**4*lum)**0.5
     elif calc == 2:
-        return (radius/((T_sun/temp)**2))**2
+        return (radius/((Teff_sun/Teff)**2))**2
     elif calc == 3:
-        return T_sun/(radius/lum**0.5)**0.5
+        return Teff_sun/(radius/lum**0.5)**0.5
     else:
         return 'ERROR!!!'
 
 def AD(dist, angular_diameter):
-    return dist * angular_diameter/1000 * 107.5
+    return dist*angular_diameter/1000*107.5
 
 def log(param):
     return log10(param/1)
 
+def L_Mbol(calc: int, lum, Mbol):
+    if calc == 1:
+        return 10**(0.4*(Mbol_sun-Mbol))
+    elif calc == 2:
+        return Mbol_sun-2.5*log10(lum)
+    else:
+        return 'ERROR!'
+
 while 1:
     print('-----------------------------------------------------------')
-    print('       Xterminal1 : Stellar characteristics calculator')
+    print('           Xterminal1 : Stellar data calculator')
     print('-----------------------------------------------------------')
 
     calcs = [
@@ -53,25 +61,25 @@ while 1:
             match radius_calc:
                 # Luminosity-effective temperature
                 case 1:
-                    radius = L_R_Teff(1, 0, float(input('Effective temperature [K]: ')), float(input('Luminosity [L☉]: ')))
+                    radius = LRT(1, 0, float(input('Effective temperature, Teff [K]: ')), float(input('Luminosity, L [L☉]: ')))
                     print()
                     print(f'Radius [R☉]: {radius}')
 
                 # Distance-angular diameter
                 case 2:
-                    radius = AD(float(input('Distance [pc]: ')), float(input('Angular diameter [mas]: ')))
+                    radius = AD(float(input('Distance, d [pc]: ')), float(input('Angular diameter, AD [mas]: ')))
                     print()
                     print(f'Radius [R☉]: {radius}')
 
                 # Parallax-angular diameter
                 case 3:
-                    radius = AD(1/float(input('Parallax [mas]: '))*1000, float(input('Angular diameter [mas]: ')))
+                    radius = AD(1/float(input('Parallax, Plx [mas]: '))*1000, float(input('Angular diameter, AD [mas]: ')))
                     print()
                     print(f'Radius [R☉]: {radius}')
 
                 # log(R/R☉)
                 case 4:
-                    log_radius = log(float(input('Radius [R☉]: ')))
+                    log_radius = log(float(input('Radius, R [R☉]: ')))
                     print()
                     print(f'log(R/R☉): {log_radius}')
 
@@ -80,7 +88,7 @@ while 1:
         # Luminosity
         case 2:
             lum_calcs = [
-                'Radius-effective temperature', 'log(L/L☉)'
+                'Radius-effective temperature', 'log(L/L☉)', 'Bolometric magnitude'
             ]
             for count, ele in enumerate(lum_calcs, 1):
                 print(count, ele)
@@ -90,6 +98,46 @@ while 1:
             match lum_calc:
                 # Radius-effective temperature
                 case 1:
-                    lum = L_R_Teff(2, float(input('Radius [R☉]: ')), float(input('Effective temperature [K]: ')), 0)
+                    lum = LRT(2, float(input('Radius, R [R☉]: ')), float(input('Effective temperature, Teff [K]: ')), 0)
                     print()
-                    print(f'Luminosity [L☉]: {lum}')
+                    print(f'Luminosity, L [L☉]: {lum}')
+                
+                # log(L/L☉)
+                case 2:
+                    log_lum_calcs = ['Calculating luminosity with log(L/L☉)', 'Calculating log(L/L☉) with luminosity']
+                    for count, ele in enumerate(log_lum_calcs, 1):
+                        print(count, ele)
+                    print()
+                    log_lum_calc = int(input(">>> "))
+
+                    match log_lum_calc:
+                        # Calculating luminosity with log(L/L☉)
+                        case 1:
+                            pass
+
+                        # Calculating log(L/L☉) with luminosity
+                        case 2:
+                            log_lum = log(float(input('Luminosity, L [L☉]: ')))
+                            print()
+                            print(f'log(L/L☉): {log_lum}')
+                
+                # Mbol
+                case 3:
+                    Mbol_lum_calcs = ['Calculating luminosity with bolometric magnitude', 'Calculating bolometric magnitude with luminosity']
+                    for count, ele in enumerate(Mbol_lum_calcs, 1):
+                        print(count, ele)
+                    print()
+                    Mbol_lum_calc = int(input('>>> '))
+
+                    match Mbol_lum_calc:
+                        # Calculating luminosity with bolometric magnitude
+                        case 1:
+                            lum = L_Mbol(1, 0, float(input('Bolometric magnitude, Mbol: ')))
+                            print()
+                            print(f'Luminosity, L [L☉]: {lum}')
+
+                        # Calculating bolometric magnitude with luminosity
+                        case 2:
+                            Mbol = L_Mbol(2, float(input('Luminosity, L [L☉]: ')), 0)
+                            print()
+                            print(f'Bolometric magnitude, Mbol: {Mbol}')
